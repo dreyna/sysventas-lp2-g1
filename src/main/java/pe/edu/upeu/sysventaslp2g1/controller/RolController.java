cf12a6596e85fb94c097c22fd30884940e149a5d
David <reyna_b@upeu.edu.pe>
2020-09-22 22:09:17 -0500

    create rol

--- src/main/java/pe/edu/upeu/sysventaslp2g1/controller/RolController.java
+++ src/main/java/pe/edu/upeu/sysventaslp2g1/controller/RolController.java
@@ -6,7 +6,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,10 @@ public Rol read(@PathVariable int id ) {
 @DeleteMapping("/delete/{id}")
 public int delete(@PathVariable int id ) {
 	return rolService.delete(id);
+}
+@PostMapping("/rol")
+public int create(@RequestBody Rol r) {
+	System.out.println("Dato"+r.getNomrol());
+	return rolService.create(r);
 }
 }
