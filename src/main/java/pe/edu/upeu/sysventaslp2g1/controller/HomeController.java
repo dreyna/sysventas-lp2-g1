ef1b7b0f1efc26e56c8bf6349c1b219302ed1630
David Reyna Barreto <reyna_b@upeu.edu.pe>
2020-09-24 21:23:32 -0500

    a111

--- src/main/java/pe/edu/upeu/sysventaslp2g1/controller/HomeController.java
+++ src/main/java/pe/edu/upeu/sysventaslp2g1/controller/HomeController.java
@@ -13,5 +13,13 @@ public class HomeController {
 	public String mensaje2() {
 		return "Bienvenidos a Microservicios Síncronos";
 	}
+	@GetMapping("/hola2")
+	public String mensaje3() {
+		return "Bienvenidos a Microservicios Síncronos";
+	}
+	@GetMapping("/hola1")
+	public String mensaje4() {
+		return "Bienvenidos a Microservicios Síncronos";
+	}
 
 }
